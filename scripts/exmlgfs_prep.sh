66e522a325bdec01d77ee79793ff617fd5c845b5
Russell Manser <russell.manser@noaa.gov>
2025-08-21 16:05:03 +0000

    Sleep for GFS input for faster job completion
    Previous ecFlow trigger failed. Trigger set to active/complete
    on GFS post job which completes in ~10 minutes. Required file
    arrives much sooner, so use sleep.
    
    Remove python call to conform with implementation standards

--- scripts/exmlgfs_prep.sh
+++ scripts/exmlgfs_prep.sh
@@ -3,15 +3,31 @@ set -x
 
 echo "Starting $0"
 
+# Wait for up to 15 minutes to find the latest GFS 6-h forecast
+target=$COMINgfs/gfs.t${cyc}z.pgrb2.0p25.f006
+n_tries=30
+for counter in $( seq 1 1 $n_tries ); do
+    if [[ -e "$target" ]]; then
+        echo "Required file found: $target"
+        sleep 30
+        break
+    elif [[ $counter -eq $n_tries ]]; then
+        echo "Required file not found after $counter tries: $target"
+        export err=1; err_chk
+    fi
+
+    sleep 30
+done
+
 # copy data in
-cpreq $COMINgfs/gfs.t${cyc}z.pgrb2.0p25.f000 $DATA/data/
-cpreq $COMINgfs/gfs.t${cyc}z.pgrb2.0p25.f006 $DATA/data/
 cpreq $COMINgfsm1/gfs.t${prev_cyc}z.pgrb2.0p25.f000 $DATA/data/
 cpreq $COMINgfsm1/gfs.t${prev_cyc}z.pgrb2.0p25.f006 $DATA/data/
-ls -l $DATA/data/ 
+cpreq $COMINgfs/gfs.t${cyc}z.pgrb2.0p25.f000 $DATA/data/
+cpreq $COMINgfs/gfs.t${cyc}z.pgrb2.0p25.f006 $DATA/data/
+ls -l $DATA/data/
 
 # run gfs_utility script to create graphcast input
-python $USHmlgfs/gfs_utility.py "$prev_PDYHH" "$curr_PDYHH" -o output
+$USHmlgfs/gfs_utility.py "$prev_PDYHH" "$curr_PDYHH" -o output
 export err=$?; err_chk
 
 cpfs output/source-gfs_date-${curr_PDYHH}_res-0.25_levels-13_steps-3.nc $COMOUT
