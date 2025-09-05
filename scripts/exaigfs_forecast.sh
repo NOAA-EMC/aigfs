7b535eb143794c22e5b2d9a005e8afe74e4f0008
Russell Manser <russell.manser@noaa.gov>
2025-09-05 19:44:50 +0000

    Rename mlgfs->aigfs and add dbnet_alerts

---
+++ scripts/exaigfs_forecast.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -x
+
+echo "Starting $0"
+
+mkdir -p ${DATA}/data
+
+# copy data in
+cpreq $COMIN/aigfs.${cycle}.ic.nc $DATA/data
+ls -l $DATA/data
+
+# run graphcast forecast
+$USHaigfs/run_graphcast.py \
+	-n mlgfs \
+	-i ${DATA}/data/aigfs.${cycle}.ic.nc \
+	-o ${DATA} \
+	-w $FIXaigfs \
+	-l 64
+export err=$?; err_chk
+
+for fh in $( seq -f "%03g" 0 6 384 ); do
+  file=$COMOUT/aigfs.${cycle}.pres.0p25.f${fh}.grib2
+  cpfs mlgfs.${cycle}.pres.0p25.f${fh}.grib2 ${file}
+  cpfs mlgfs.${cycle}.pres.0p25.f${fh}.grib2.idx ${file}.idx
+
+  if [ "$SENDDBN" = "YES" ]; then 
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2 $job ${file}
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2_IDX $job ${file}.idx
+  fi
+
+  file=$COMOUT/aigfs.${cycle}.sfc.0p25.f${fh}.grib2
+  cpfs mlgfs.${cycle}.sfc.0p25.f${fh}.grib2 ${file}
+  cpfs mlgfs.${cycle}.sfc.0p25.f${fh}.grib2.idx ${file}.idx
+
+  if [ "$SENDDBN" = "YES" ]; then 
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2 $job ${file}
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2_IDX $job ${file}.idx
+  fi
+done
+
+echo "$0 completed normally"
