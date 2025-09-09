79fe0111327fa5ddf1aeb112f011d23c5639b551
Russell Manser <russell.manser@noaa.gov>
2025-09-09 18:52:04 +0000

    Update output file names

--- scripts/exaigfs_forecast.sh
+++ scripts/exaigfs_forecast.sh
@@ -19,22 +19,22 @@ $USHaigfs/run_graphcast.py \
 export err=$?; err_chk
 
 for fh in $( seq -f "%03g" 0 6 384 ); do
-  file=$COMOUT/aigfs.${cycle}.pres.0p25.f${fh}.grib2
-  cpfs mlgfs.${cycle}.pres.0p25.f${fh}.grib2 ${file}
-  cpfs mlgfs.${cycle}.pres.0p25.f${fh}.grib2.idx ${file}.idx
+  file=aigfs.${cycle}.pres.f${fh}.grib2 
+  cpfs ${file} $COMOUT
+  cpfs ${file}.idx $COMOUT
 
   if [ "$SENDDBN" = "YES" ]; then 
-    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2 $job ${file}
-    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2_IDX $job ${file}.idx
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2 $job $COMOUT/${file}
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2_IDX $job $COMOUT/${file}.idx
   fi
 
-  file=$COMOUT/aigfs.${cycle}.sfc.0p25.f${fh}.grib2
-  cpfs mlgfs.${cycle}.sfc.0p25.f${fh}.grib2 ${file}
-  cpfs mlgfs.${cycle}.sfc.0p25.f${fh}.grib2.idx ${file}.idx
+  file=aigfs.${cycle}.sfc.f${fh}.grib2
+  cpfs ${file} $COMOUT
+  cpfs ${file}.idx $COMOUT
 
   if [ "$SENDDBN" = "YES" ]; then 
-    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2 $job ${file}
-    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2_IDX $job ${file}.idx
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2 $job $COMOUT/${file}
+    $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2_IDX $job $COMOUT/${file}.idx
   fi
 done
 
