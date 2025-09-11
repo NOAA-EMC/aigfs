a434b69056b79331e20c47f20ea7262c449a8df8
Russell Manser <russell.manser@noaa.gov>
2025-09-11 20:45:15 +0000

    Cleanup dead code from name changes

--- scripts/exaigfs_forecast.sh
+++ scripts/exaigfs_forecast.sh
@@ -19,6 +19,9 @@ $USHaigfs/run_graphcast.py \
 export err=$?; err_chk
 
 for fh in $( seq -f "%03g" 0 6 384 ); do
+  #file=$COMOUT/aigfs.${cycle}.pres.f${fh}.grib2
+  #cpfs mlgfs.${cycle}.pres.0p25.f${fh}.grib2 ${file}
+  #cpfs mlgfs.${cycle}.pres.0p25.f${fh}.grib2.idx ${file}.idx
   file=aigfs.${cycle}.pres.f${fh}.grib2 
   cpfs ${file} $COMOUT
   cpfs ${file}.idx $COMOUT
@@ -28,6 +31,9 @@ for fh in $( seq -f "%03g" 0 6 384 ); do
     $DBNROOT/bin/dbn_alert MODEL AIGFS_GB2_IDX $job $COMOUT/${file}.idx
   fi
 
+  #file=$COMOUT/aigfs.${cycle}.sfc.f${fh}.grib2
+  #cpfs mlgfs.${cycle}.sfc.0p25.f${fh}.grib2 ${file}
+  #cpfs mlgfs.${cycle}.sfc.0p25.f${fh}.grib2.idx ${file}.idx
   file=aigfs.${cycle}.sfc.f${fh}.grib2
   cpfs ${file} $COMOUT
   cpfs ${file}.idx $COMOUT
