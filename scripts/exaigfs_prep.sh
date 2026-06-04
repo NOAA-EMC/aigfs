370d5446190d9856cb14d65295edeb2db5effe11
LINLIN CUI <linlin.cui@dlogin03.dogwood.wcoss2.ncep.noaa.gov>
2026-06-04 15:08:31 +0000

    updated gfsv17 file name and directory

--- scripts/exaigfs_prep.sh
+++ scripts/exaigfs_prep.sh
@@ -4,7 +4,8 @@ set -x
 echo "Starting $0"
 
 # Wait for up to 15 minutes to find the latest GFS 6-h forecast
-target=$COMINgfs/gfs.t${cyc}z.pgrb2.0p25.f000
+# gfs.t${cyc}z.pres_a.0p25.${fh}.grib2
+target=$COMINgfs/gfs.t${cyc}z.pres_a.0p25.f000.grib2
 n_tries=30
 for counter in $( seq 1 1 $n_tries ); do
     if [[ -e "$target" ]]; then
@@ -20,10 +21,11 @@ for counter in $( seq 1 1 $n_tries ); do
 done
 
 # copy data in
-cpreq $COMINgfsm2/gfs.t${m2_cyc}z.pgrb2.0p25.f006 $DATA/data/
-cpreq $COMINgfsm1/gfs.t${m1_cyc}z.pgrb2.0p25.f000 $DATA/data/
-cpreq $COMINgfsm1/gfs.t${m1_cyc}z.pgrb2.0p25.f006 $DATA/data/
-cpreq $COMINgfs/gfs.t${cyc}z.pgrb2.0p25.f000 $DATA/data/
+# gfs.t${cyc}z.pres_a.0p25.${fh}.grib2
+cpreq $COMINgfsm2/gfs.t${m2_cyc}z.pres_a.0p25.f006 $DATA/data/
+cpreq $COMINgfsm1/gfs.t${m1_cyc}z.pres_a.0p25.f000 $DATA/data/
+cpreq $COMINgfsm1/gfs.t${m1_cyc}z.pres_a.0p25.f006 $DATA/data/
+cpreq $COMINgfs/gfs.t${cyc}z.pres_a.0p25.f000 $DATA/data/
 ls -l $DATA/data/
 
 # run gfs_utility script to create graphcast input
